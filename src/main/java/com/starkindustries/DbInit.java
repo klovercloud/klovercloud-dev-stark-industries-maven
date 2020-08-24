4ae90c729bef8d466830bc602184d00d9c5a33f1
zeromsi <www.zeromsi2@gmail.com>
2020-08-24 23:13:31 +0600

    Disable postconstruct

--- src/main/java/com/starkindustries/DbInit.java
+++ src/main/java/com/starkindustries/DbInit.java
@@ -13,20 +13,20 @@ public class DbInit {
 	@Autowired
 	AvengersRepository avengersRepo;
 	
-    @PostConstruct
-    private void postConstruct() {
-    	ArrayList<String> missions=new ArrayList();
-    	missions.add("Civil war");
-    	missions.add("Infinity war");
-        Avengers tonyStark = new Avengers("101", "Tony Stark", "Chairman", "tonystark.jpg", missions);
-        avengersRepo.save(tonyStark);
-        
-        
-    	missions=new ArrayList();
-    	missions.add("Civil war");
-    	missions.add("Infinity war");
-        Avengers  steve = new Avengers("102", "Steve Rogers", "Vice Chairman", "steve.jpg", missions);
-        avengersRepo.save(steve);
-    }
+//    @PostConstruct
+//    private void postConstruct() {
+//    	ArrayList<String> missions=new ArrayList();
+//    	missions.add("Civil war");
+//    	missions.add("Infinity war");
+//        Avengers tonyStark = new Avengers("101", "Tony Stark", "Chairman", "tonystark.jpg", missions);
+//        avengersRepo.save(tonyStark);
+//        
+//        
+//    	missions=new ArrayList();
+//    	missions.add("Civil war");
+//    	missions.add("Infinity war");
+//        Avengers  steve = new Avengers("102", "Steve Rogers", "Vice Chairman", "steve.jpg", missions);
+//        avengersRepo.save(steve);
+//    }
 	
 }
