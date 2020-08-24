a975c79c66abebb67d56f5469847d4f3c3997eab
zeromsi <www.zeromsi2@gmail.com>
2020-08-24 20:02:03 +0600

    add health check api

--- src/main/java/com/starkindustries/Application.java
+++ src/main/java/com/starkindustries/Application.java
@@ -6,13 +6,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
  
 @SpringBootApplication
 @EnableCaching
+
+@RestController
 public class Application{
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	@GetMapping("api/v1/health")
+	public String getHealth() {
+		
+		return "Hello from stark industries!";
+	}
 }
+
+
